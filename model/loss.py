500652cf239febc96a7bb1602f013130797ca06f
Arecib0 <d22meng@uwaterloo.ca>
2023-11-30 22:49:06 -0500

    Switched loss from tensorflow to pytorch

--- model/loss.py
+++ model/loss.py
@@ -1,15 +1,21 @@
-from keras import backend as K
+import torch
+import torch.nn.functional as F
 
 def cross_entropy(y_true, y_pred):
-    return K.categorical_crossentropy(y_true, y_pred)
+    return F.cross_entropy(y_pred, y_true)
+    # Note: F.cross_entropy combines log_softmax and nll_loss
+    # Because of this, y_pred should be the raw output of the model
+    # Do not apply softmax in the last layer of the model
+    # It also expects y_true to be a 1D tensor of class indices
+
 
 def adaptive_clustering(y_true, y_pred):
     # Replace this with your actual implementation
-    return K.mean(y_pred - y_true)
+    return torch.mean(y_pred - y_true)
 
 def entropy_separation(y_true, y_pred):
     # Replace this with your actual implementation
-    return K.mean(y_pred - y_true)
+    return torch.mean(y_pred - y_true)
 
 def combined_loss(y_true, y_pred):
     return cross_entropy(y_true, y_pred) + adaptive_clustering(y_true, y_pred) + entropy_separation(y_true, y_pred)
