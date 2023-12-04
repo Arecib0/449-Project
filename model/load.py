2fc1b65dea34df577eb0423da62e1c792a59f4e7
Eric-McFarlane <147740320+Eric-McFarlane@users.noreply.github.com>
2023-12-03 19:39:51 -0500

    Update load.py
    Adding more docstring and comments to load_labels

--- model/load.py
+++ model/load.py
@@ -19,10 +19,10 @@ def load_and_preprocess_data(filepath):
         Resize((224, 224)),  # Resize images to 224x224
         ToTensor(),  # Convert to PyTorch tensor
         Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),  # Normalize data
-    ])
+    ]) # normalize basically scales RGB channels using values determined from a large collection of images. Makes z-score
         # Apply the preprocessing steps to each image
     data = torch.stack([preprocess(Image.fromarray(img.astype('uint8'))) for img in data])
-    # Change data from (N, H, W, C) to (N, C, H, W)
+    # Change data from (N, H, W, C) to (N, C, H, W); the original format
     data = data.permute(0, 1, 3, 2)
     print(data.shape)
 
@@ -30,14 +30,20 @@ def load_and_preprocess_data(filepath):
 
 def load_labels(filepath):
     """
-    Loads labels from filepath in a one-hot encoded format (i.e. [0, 1, 0] for class 2)
+    Loads labels from filepath in a one-hot encoded format (i.e. [0, 1, 0] for class 2).
+    Possible labels are [1,0,0],[0,1,0],[0,0,1] for the 3 class problem.
     
     :param filepath: Path to labels
-    :return: Labels in form of PyTorch tensor of shape (N,)
+    :return: Labels in form of PyTorch tensor of shape (1,N). The ith entry is the index
+    in the ith sample with 1 as its entry.
+
+    Ex.
+    labels=np.load()=[[1,0,0],[0,1,0],[0,0,1]] -> torch([0,1,2])
+    
     """
     labels = np.load(filepath) 
     labels = torch.tensor(labels, dtype=torch.long)
-    labels = torch.argmax(labels, dim=1)  # Convert one-hot encoding to class number
+    labels = torch.argmax(labels, dim=1)  # Convert one-hot encoding to class number 0,1, or 2 in 3 class problem
     print(labels.shape)
 
     return labels
