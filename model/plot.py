678f3cb746d2f808f4eef8178a9090b063536f12
Eric-McFarlane <147740320+Eric-McFarlane@users.noreply.github.com>
2023-12-04 00:14:57 -0500

    Update plot.py
    Loss plotting docstring added

--- model/plot.py
+++ model/plot.py
@@ -3,6 +3,14 @@ import numpy as np
 
 
 def plotLoss(ce_losses, ac_losses, es_losses):
+        '''
+        Generates a bunch of loss plots and saves them to the ouput folder.
+        ce_losses is the cross entropy losss, ac_losses is binary cross entropy loss,
+        and es_losses is entropy separation loss. These are losses over all the epochs used by the model.
+        The respective names of these plots are: ce_loss_plot.png, ac_loss_plot.png, and
+        es_loss_plot.png. A plot of all of them in the same figure is also generated and
+        called loss_plot.png.
+        '''
         # Combined Loss chart
     plt.figure(figsize=(10, 5))
     plt.plot(ce_losses, label='CE')
